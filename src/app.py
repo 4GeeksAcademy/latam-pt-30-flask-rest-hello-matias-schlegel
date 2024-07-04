f2e8498f97804e61ef527e39e71c5dac8380cfee
matischlegel <161637922+matischlegel@users.noreply.github.com>
2024-07-04 00:40:29 +0000

    proyecto

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planet, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,136 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+
+
+
+
+
+
+
+
+
+
+@app.route('/people', methods=['GET'])
+def get_people():
+    people = People.query.all()
+    return jsonify([{'id': person.id, 'name': person.name} for person in people])
+    
+    
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_people_id(people_id):
+    person = People.query.get_or_404(people_id)
+    return jsonify({'id': person.id, 'name': person.name})
+
+
+
+
+    
+@app.route('/planet', methods=['GET'])
+def get_planet():
+    planets = Planet.query.all()
+    return jsonify([{'id': planet.id, 'name': planet.name} for planet in planets])
+    
+    
+@app.route('/planet/<int:planet_id>', methods=['GET'])
+def get_planet_id(planet_id):
+    planet = Planet.query.get_or_404(planet_id)
+    return jsonify({'id': planet.id, 'name': planet.name})
+    
+
+
+
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify(
+        [
+            {
+                'id': user.id,
+                'username': user.username,
+                'email': user.email,
+                'is_active': user.is_active
+            }
+            for user in users
+        ]
+    )
+    
+@app.route('/users/favorites', methods=['GET'])
+def get_user_favorite(user_id):
+    user_id = 1 
+    user = User.query.get_or_404(user_id) #busca al usuairo si no se encuentra devuelve un error 404
+    # favorite.serialize() se llama en cada objeto Favorite para convertirlo en un diccionario JSON
+    #  user.favorites devuelve una lista de objetos Favorite que pertenecen al usuario.
+    return jsonify([favorite.serialize() for favorite in user.favorites]) 
+
+
+
+
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def get_favorite_planet(planet_id):
+    user_id = 1  # Ajustar para obtener el ID del usuario actual
+    favorite = Favorite(user_id=user_id, planet_id=planet_id)
+    db.session.add(favorite) # Añade el nuevo objeto favorite a la sesión de la base de datos.
+    db.session.commit() # Guarda los cambios en la base de datos, lo que significa que el nuevo favorito se inserta en la tabla 
+    return jsonify(favorite.serialize()), 201 # Este es el código de estado HTTP para "Created"
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def get_favorite_people(people_id):
+    user_id = 1  # Ajustar para obtener el ID del usuario actual
+    favorite = Favorite(user_id=user_id, people_id=people_id)
+    db.session.add(favorite) # Añade el nuevo objeto favorite a la sesión de la base de datos.
+    db.session.commit() # Guarda los cambios en la base de datos, lo que significa que el nuevo favorito se inserta en la tabla 
+    return jsonify(favorite.serialize()), 201 # Este es el código de estado HTTP para "Created"
+
+
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_favorite_planet(planet_id):
+    user_id = 1
+    favorite = Favorite.query.filter_by(user_id=user_id, planet_id=planet_id).first_or_404()
+    db.session.delete(favorite) # Añade la operación de eliminación del objeto favorite a la sesión de la base de datos.
+    db.session.commit()
+    return jsonify({"message": "favorito eliminado"}), 200
+
+@app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
+def delete_favorite_people(people_id):
+    data = request.get_json()  # Obtener datos JSON del cuerpo de la solicitud
+    user_id = data.get('user_id')  # Obtener el user_id desde los datos de la solicitud
+    
+    if not user_id:
+        return jsonify({"error": "user is required"}), 400
+    
+    favorite = Favorite.query.filter_by(user_id=user_id, people_id=people_id).first_or_404()
+    db.session.delete(favorite) # Añade la operación de eliminación del objeto favorite a la sesión de la base de datos.
+    db.session.commit()
+    return jsonify({"message": "favorito eliminado"}), 200
+
+
+@app.route('/create_users', methods=['POST'])
+def create_user():
+    data = request.get_json()  # Obtener los datos JSON del cuerpo de la solicitud
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+    is_active = data.get('is_active', True)
+
+    if not username or not email or not password:
+        return jsonify({"error": "Username, email, and password are required"}), 400
+
+    new_user = User(username=username, email=email, password=password, is_active=is_active)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({
+        "id": new_user.id,
+        "username": new_user.username,
+        "email": new_user.email,
+        "is_active": new_user.is_active
+    }), 201
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,16 +4,51 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship('Favorite', backref='user')
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    def serialize(self): #no entendi mucho para que sirve
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "is_active": self.is_active
+        }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'), nullable=True)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "people_id": self.people_id,
+            "planet_id": self.planet_id
             # do not serialize the password, its a security breach
         }
